6e0f75beab8481ea12dda9f6619434b9f66f058f
Vugar Salehov <salehov@gmail.com>
2015-06-05 16:42:24 +0500

    Create messages_az.js

---
+++ src/localization/messages_az.js
@@ -0,0 +1,23 @@
+/*
+ * Translated default messages for the jQuery validation plugin.
+ * Locale: AZ (Azerbaijani; Azərbaycan dili)
+ */
+$.extend($.validator.messages, {
+	required: "Bu sahənin doldurulması mütləqdir.",
+	remote: "Zəhmət olmasa, düzəltin.",
+	email: "Zəhmət olmasa, elektron poçt ünvanını düzgün daxil edin.",
+	url: "Zəhmət olmasa, elektron veb ünvanı (URL) düzgün daxil edin.",
+	date: "Zəhmət olmasa, tarixi düzgün daxil edin.",
+	dateISO: "Zəhmət olmasa, tarixi İSO standartına uyğun daxil edin.",
+	number: "Zəhmət olmasa, ədədi düzgün daxil edin.",
+	digits: "Zəhmət olmasa, yalnız rəqəm daxil edin.",
+	creditcard: "Zəhmət olmasa, kredit kartının nömrəsini düzgün daxil edin.",
+	equalTo: "Zəhmət olmasa, eyni informasiyanı yenidən daxil edin.",
+	extension: "Zəhmət olmasa, uyğun genişlənmədə olan fayl seçin.",
+	maxlength: $.validator.format("Zəhmət olmasa, {0} simvoldan artıq daxil etməyin."),
+	minlength: $.validator.format("Zəhmət olmasa, {0} simvoldan az daxil etməyin."),
+	rangelength: $.validator.format("Zəhmət olmasa, {0} - {1} sayları aralığında simvol daxil edin."),
+	range: $.validator.format("Zəhmət olmasa, {0} - {1} sayları aralığında ədəd daxil edin."),
+	max: $.validator.format("Zəhmət olmasa, {0}-dən/dan kiçik və ya bərabər ədəd daxil edin."),
+	min: $.validator.format("Zəhmət olmasa, {0}-dən/dan böyük və ya bərabər ədəd daxil edin.")
+});
